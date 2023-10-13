21727f4dbaedad4fb035b3c848d9b70fdc71af32
Jibran <mac@MacBook-Pro-2.lan>
2023-10-13 14:59:03 +0700

    feat: containerize the apps

---
+++ todoapp/test.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+print(ROOT_BASE_DIR);
